b1f3b60b74cd072a422a4137da6812dd51c45b1a
Bakshish Singh <bakshish@Bakshishs-MacBook-Pro.local>
2020-06-24 02:32:56 +0530

    minor fix

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,47 +1,49 @@
-// Declarations
-var names = [
-  "Yaakov",
-  "John",
-  "Jen",
-  "Jason",
-  "Paul",
-  "Frank",
-  "Larry",
-  "Paula",
-  "Laura",
-  "Jim",
-];
-var i = 0;
-var list = document.getElementById("namesList");
+(function main() {
+  var names = [
+    "Yaakov",
+    "John",
+    "Jen",
+    "Jason",
+    "Paul",
+    "Frank",
+    "Larry",
+    "Paula",
+    "Laura",
+    "Jim",
+  ];
+  var i = 0;
+  var list = document.getElementById("namesList");
 
-// Actual Solution
-for (name in names) {
-  if (names[name][0].toLowerCase() === "j") {
-    byeSpeaker.speak(names[name]);
-  } else {
-    helloSpeaker.speak(names[name]);
-  }
-}
-
-//Webpages Loop
-function namesLoop(names) {
-  setTimeout(() => {
-    if (names[i][0].toLowerCase() === "j") {
-      var showName = document.createElement("div");
-      showName.innerHTML = `Goodbye ${names[i]}!`;
-      showName.className = "output animate goodbye";
-      list.appendChild(showName);
+  // Actual Solution
+  for (name in names) {
+    if (names[name][0].toLowerCase() === "j") {
+      byeSpeaker.speak(names[name]);
     } else {
-      var showName = document.createElement("div");
-      showName.innerHTML = `Hello ${names[i]}!`;
-      showName.className = "output animate hello";
-      list.appendChild(showName);
+      helloSpeaker.speak(names[name]);
     }
+  }
 
-    i++;
-    if (i < names.length) {
-      namesLoop(names);
-    }
-  }, 500);
-}
-namesLoop(names);
+  //Webpages Loop
+  function namesLoop(names) {
+    setTimeout(() => {
+      if (names[i][0].toLowerCase() === "j") {
+        var showName = document.createElement("div");
+        showName.innerHTML = `Goodbye ${names[i]}!`;
+        showName.className = "output animate goodbye";
+        list.appendChild(showName);
+      } else {
+        var showName = document.createElement("div");
+        showName.innerHTML = `Hello ${names[i]}!`;
+        showName.className = "output animate hello";
+        list.appendChild(showName);
+      }
+
+      i++;
+      if (i < names.length) {
+        namesLoop(names);
+      }
+    }, 500);
+  }
+  namesLoop(names);
+})();
+// Declarations
